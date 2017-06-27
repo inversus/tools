94da9bc5e651d3a902bccdb57bfa8961d16c8327
Vasco Rio <vasco.rio@gmail.com>
2017-06-27 09:47:04 +0100

    Added some more tools

---
+++ libs/prompt.sh
@@ -0,0 +1,3 @@
+# PS1="\[\`if [[ \$? = "0" ]]; then echo '\e[32m\h\e[0m'; else echo '\e[31m\h\e[0m' ; fi\`:\w\n\$" 
+PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ "
+
--- load.sh
+++ load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 source ~/tools/libs/alias.sh
+source ~/tools/libs/perforce.sh
+source ~/tools/libs/prompt.sh
 
 export VRIO_TOOLS=1
